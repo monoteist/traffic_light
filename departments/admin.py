076a478621a31cfa477e97b814e6119fa2d7ba6d
monoteist <i.ezerbaev97@gmail.com>
2021-11-24 22:27:04 +0300

    add DepartmentSerializer LegalEntitySerializer views and urls

--- departments/admin.py
+++ departments/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from departments.models import Department
 
 class DepartmentAdmin(admin.ModelAdmin):
-    def client_count(self, obj):
+    def client_count(self):
         return 1
     client_count.short_description = 'Количество клиентов'
     fields = ('title', 'legal_entity', 'client')
